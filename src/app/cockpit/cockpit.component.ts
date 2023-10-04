e402bc924e957893428f45d8dc88cd3718ee1b00
Matt Tucker <arcanemahuloq@gmail.com>
2023-10-03 21:04:33 -0500

    Initial Commit

---
+++ src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,24 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-cockpit",
+  templateUrl: "./cockpit.component.html",
+  styleUrls: ["./cockpit.component.css"],
+})
+export class CockpitComponent {
+  onAddServer() {
+    this.serverElements.push({
+      type: "server",
+      name: this.newServerName,
+      content: this.newServerContent,
+    });
+  }
+
+  onAddBlueprint() {
+    this.serverElements.push({
+      type: "blueprint",
+      name: this.newServerName,
+      content: this.newServerContent,
+    });
+  }
+}
