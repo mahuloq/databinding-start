7bcccee6de228edff68c67d18cabc914fb98dbef
Matt Tucker <arcanemahuloq@gmail.com>
2023-10-06 16:18:46 -0500

    Modified code to try out all Lifecycle Hooks, Change First Element doesnt work same as video

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,14 +1,71 @@
-import { Component, OnInit, Input } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  Input,
+  ViewEncapsulation,
+  OnChanges,
+  SimpleChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy,
+} from "@angular/core";
 
 @Component({
   selector: "app-server-element",
   templateUrl: "./server-element.component.html",
   styleUrls: ["./server-element.component.css"],
+  // encapsulation: ViewEncapsulation.None
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent
+  implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    OnDestroy
+{
   @Input("srvElement") element: { type: string; name: string; content: string };
 
-  constructor() {}
+  @Input() name: string;
 
-  ngOnInit() {}
+  constructor() {
+    console.log("constructer called");
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log("ngOnChanges called");
+    console.log(changes);
+  }
+
+  ngOnInit() {
+    console.log("ngOnInit called");
+  }
+
+  ngDoCheck(): void {
+    console.log("ngDoCheck Called");
+  }
+
+  ngAfterContentInit(): void {
+    console.log("ngAfterContentInit Called");
+  }
+
+  ngAfterContentChecked(): void {
+    console.log("ngAfterContentChecked Called");
+  }
+
+  ngAfterViewInit(): void {
+    console.log("ngAfterViewInit Called");
+  }
+
+  ngAfterViewChecked(): void {
+    console.log("ngAfterViewChecked Called");
+  }
+
+  ngOnDestroy(): void {
+    console.log("ngOnDestroy Called");
+  }
 }
